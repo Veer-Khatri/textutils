2817c4be23f52e62ed08005da882f5a2c2332e44
Veer Khatri <veerkhatri70@gmail.com>
2021-08-28 13:51:07 +0530

    react hs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 
 function App() {
   return (
+    // the below code is JSX which is a combination of js and html
+    // we only have to return one tag and if we want to return more than one tag we use JSX fragment which are these --> <> </>
+    <>
+    <h1>I am Heading which is in different tag and not giving error because of JSX fragment</h1>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +23,7 @@ function App() {
         </a>
       </header>
     </div>
+    </>
   );
 }
 
