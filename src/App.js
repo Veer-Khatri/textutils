d99e2c2a1c33779af83eb541d4ea72bbaa55fa15
Veer Khatri <veerkhatri70@gmail.com>
2021-08-29 14:25:28 +0530

    learning

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import veer_logo from './veer_logo.png';
 import './App.css';
 
 function App() {
@@ -6,23 +6,21 @@ function App() {
     // the below code is JSX which is a combination of js and html
     // we only have to return one tag and if we want to return more than one tag we use JSX fragment which are these --> <> </>
     <>
-    <h1>I am Heading which is in different tag and not giving error because of JSX fragment</h1>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav className="navbar">
+      <div className="logo_img">
+      <a href="/" id="logo_anchor_tag"><img src={veer_logo} alt="logo" id='logo_img'/></a> 
+      </div>
+      <div className="links">
+        <li><a href="/" className="navlinks">Home</a></li>
+        <li><a href="/" className="navlinks">About</a></li>
+        <li><a href="/" className="navlinks">Contact Us</a></li>
+        <li><a href="/" className="navlinks">Become a Seller</a></li>
+        </div>
+      <div className="searchBox">
+        <input type="text" id="search_input"/>
+        <button id="search_btn">Search</button>
+      </div>
+    </nav>
     </>
   );
 }
